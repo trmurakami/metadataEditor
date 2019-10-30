ed0097c7f4c29647fade6cc4cba922a907e7e556
Tiago Murakami <trmurakami@gmail.com>
2019-10-29 22:42:10 -0300

    Initial

--- rppbci.php
+++ rppbci.php
@@ -4,5 +4,18 @@ require 'functions.php';
 
 print("<pre>".print_r($_REQUEST, true)."</pre>");
 
+$body["doc"]["name"] = $_REQUEST["name"];
+$body["doc"]["alternateName"] = $_REQUEST["alternateName"];
+$body["doc"]["doi"] = $_REQUEST["doi"];
+$body["doc"]["isPartOf"]["name"] = $_REQUEST["isPartOf_name"];
+$body["doc"]["isPartOf"]["ISSN"] = $_REQUEST["isPartOf_ISSN"];
+$body["doc"]["isPartOf"]["name"] = $_REQUEST["isPartOf_name"];
+$body["doc"]["isPartOf"]["name"] = $_REQUEST["isPartOf_name"];
+$body["doc"]["isPartOf"]["name"] = $_REQUEST["isPartOf_name"];
+
+
+
+print("<pre>".print_r($body, true)."</pre>");
+
 
 ?>
